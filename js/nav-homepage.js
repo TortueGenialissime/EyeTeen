55e7fa858908bdcc457e940d031d13bbb3af1352
Paul Le Gall <paullegall28@icloud.com>
2018-01-22 10:02:40 +0100

    improve navigation + add nav actions

---
+++ js/nav-homepage.js
@@ -0,0 +1,24 @@
+$(function(){
+    Array.prototype.forEach.call(document.querySelectorAll("#social-nav ul li"), function(elm){
+        elm.addEventListener("click", function(e){
+           if(elm.dataset.action == "login"){
+               document.querySelector('#active-tab').style.left = 0; 
+                $("#signup-box").css("left", "-50%");
+                $("#login-box").css("left", "0%");
+               $("#signup-box").fadeOut("slow", function(){
+                    $("#login-box").fadeIn("slow", function(){ 
+                    });
+               });
+           }
+            else {
+                document.querySelector('#active-tab').style.left = "50%"; 
+                $("#signup-box").css("left", "0%");
+                $("#login-box").css("left", "50%");
+               $("#login-box").fadeOut("slow", function(){
+                    $("#signup-box").fadeIn("slow", function(){ 
+                    });
+               });
+            }
+        });
+    });
+});
