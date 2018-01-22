7b6c9859e38d5da9ef97f55b5adb7458e6e3cb74
Paul Le Gall <paullegall28@icloud.com>
2018-01-22 15:29:37 +0100

    WIP

--- js/nav-homepage.js
+++ js/nav-homepage.js
@@ -3,7 +3,7 @@ $(function(){
         elm.addEventListener("click", function(e){
            if(elm.dataset.action == "login"){
                document.querySelector('#active-tab').style.left = 0; 
-                $("#signup-box").css("left", "-50%");
+                $("#signup-box").css("left", "50%");
                 $("#login-box").css("left", "0%");
                $("#signup-box").fadeOut("slow", function(){
                     $("#login-box").fadeIn("slow", function(){ 
@@ -13,7 +13,7 @@ $(function(){
             else {
                 document.querySelector('#active-tab').style.left = "50%"; 
                 $("#signup-box").css("left", "0%");
-                $("#login-box").css("left", "50%");
+                $("#login-box").css("left", "-50%");
                $("#login-box").fadeOut("slow", function(){
                     $("#signup-box").fadeIn("slow", function(){ 
                     });
